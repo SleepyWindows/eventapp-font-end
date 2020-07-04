02e36d059ffaa0f154b707fd91547592ddf99ba2
Lauren Birts <59886502+lbirts@users.noreply.github.com>
2020-07-04 12:45:56 -0500

    adding basic routes in app container

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 import NavBar from './containers/navBar';
-import Content from './containers/contentContainer'
+import Login from './components/auth/login';
+import Signup from './components/auth/signup';
+import Dashboard from './components/dashboard'
+import EventList from './components/eventList';
 
 class App extends React.Component {
   state = {  }
   render() { 
     return ( 
       <div className="App">
-        <NavBar/>
-        <Content/>
+        <Router>
+          <NavBar/>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/eventlist" component={EventList}/>
+        </Router>
       </div>
      );
   }
