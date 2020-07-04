5f1c5deaf8ef8dba1f41c67cf793b1d229a54977
Lauren Birts <59886502+lbirts@users.noreply.github.com>
2020-07-04 12:57:51 -0500

    Update navBar.jsx

--- src/containers/navBar.jsx
+++ src/containers/navBar.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
     return (
         <div>
             <h1>NavBar</h1>
-            <NavLink to="/login' exact>Login</NavLink>
         </div>
     );
 }
