048d2e1e88a37e61b7e37a8097960e53779a628c
lbirts <birtslauren@gmail.com>
2020-07-06 10:34:25 -0500

    updated Login/Signup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,80 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import Login from './components/auth/login.js';
+import Signup from './components/auth/signup';
+import Dashboard from './components/dashboard'
 import NavBar from './containers/navBar';
-import Content from './containers/contentContainer'
+import Content from './containers/contentContainer';
+import EventList from './components/eventList';
+
 
 class App extends React.Component {
-  state = {  }
+  state = {
+    orgs: [],
+    events: [],
+    sort: "",
+    token: "",
+    user: {}
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/organizations', {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${localStorage.token}`
+      }
+    })
+      .then(res => res.json())
+      .then(result => this.setState({orgs: result}))
+    fetch('http://localhost:3000/events', {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${localStorage.token}`
+      }
+    })
+      .then(res => res.json())
+      .then(result => this.setState({events: result}))
+  }
+
+  handleSort = (data) => {
+    this.setState({
+      sort: data
+    })
+  }
+
+  
+  eventList = () => {
+    // console.log(this.state.events.error == "Please Login")
+    if (this.state.events.error === "Please Login") {
+      return null
+    } else {
+      let sorted = [...this.state.events].filter(event => event.public === true)
+      switch (this.state.sort) {
+        case "alphabetically":
+          return sorted = sorted.sort((a, b) => {if(a.title < b.title) {return -1} else if (a.title > b.title) {return 1} else {return 0}})
+        case "date":
+          return sorted = sorted.sort((a, b) => {return a.date - b.date})
+        case "category":
+          return sorted = sorted.sort((a, b) => {if(a.category < b.category) {return -1} else if (a.category > b.category) {return 1} else {return 0}})
+        default: 
+          return sorted
+      }
+    }
+  }
+
   render() { 
+    const events = this.eventList()
     return ( 
       <div className="App">
-        <NavBar/>
-        <Content/>
+        <Router>
+          <NavBar/>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={() => <Signup orgs={this.state.orgs} />} />
+          <Route exact path="/dashboard" component={() => <Dashboard orgs={this.state.orgs} events={events} handleSort={this.handleSort} sort={this.state.sort} />} />
+          <Route exact path="/eventlist" component={EventList}/>
+        </Router>
       </div>
      );
   }
---
+++ src/components/auth/confirmStep.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Grid, Segment, Header, Button, Message } from 'semantic-ui-react';
+
+export default class infoStep extends Component {
+    state = {  }
+    render() { 
+        const {username, contact, age, supporter, role, secret_code } = this.props.values
+        return (
+            <Grid className="auth-main" style={{height: '100vh'}}>
+             <div className="auth-content">
+                <div className="auth-card">
+                    <Segment stacked>
+                     <h2>Logo</h2>
+                     <Header as="h2" color="black">
+                         Confirm Information
+                     </Header>
+                     <h4>Username: {username}</h4>
+                     <h4>Contact: {contact}</h4>
+                     <h4>Age: {age}</h4>
+                     <h4>Supporter: {supporter.join(' , ')}</h4>
+                     <h4>Role: {role}</h4>
+                     {role === "Organizer" ? <div><h4>Secret Code: {secret_code}</h4><p>Write this code down and keep it in a safe place</p></div> : null}
+
+                        
+                     <Button onClick={this.props.prevStep} style={{background: "#FF6600", "marginTop": "20px"}} size="huge">
+                        Previous
+                     </Button>
+                     <Button onClick={this.props.submit} style={{background: "#FF6600", "marginTop": "20px"}} size="huge">
+                        Submit
+                     </Button>
+        
+                     <Message size="small">
+                        <Link style={{color: '#FF6600'}} to="/login">Already Registered?</Link>
+                     </Message>
+                    </Segment>
+                 </div>
+                </div>  
+            </Grid>
+        );
+    }
+}
